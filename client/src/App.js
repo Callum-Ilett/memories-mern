36fb55d9c49f3bca9607e29eeab1c98323a61c62
Callum Ilett <cilett99@gmail.com>
2021-02-25 03:37:20 +0000

    removed test environment variable

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,6 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import { Home, Navbar, Auth } from "components";
 
-console.log("REACT_APP" + process.env.REACT_APP_GOOGLE_CLIENT_ID);
-console.log("test" + process.env.TEST);
-
 const App = () => (
   <BrowserRouter>
     <Container maxWidth="lg">
